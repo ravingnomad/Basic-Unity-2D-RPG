2671e96172c70f7fa185fea9db57461daeff86f1
tto1 <tto1@uci.edu>
2017-10-22 19:24:45 -0700

    Add files via upload

---
+++ Assets/Tommy Workspace/10_22_2017/RigidbodyAxis.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyAxis : MonoBehaviour {
+
+    public Rigidbody2D rigid;
+	// Use this for initialization
+	void Start () {
+        rigid = this.GetComponent<Rigidbody2D>();  
+    }
+
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        print("true");
+        if (coll.gameObject.tag == "Ground")
+        {
+            rigid.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePosition;
+        }
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
