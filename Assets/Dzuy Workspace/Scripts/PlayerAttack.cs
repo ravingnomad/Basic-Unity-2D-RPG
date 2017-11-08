5511b6bae8a42a8d7fe4be208181c717b91fc468
ravingnomad <joshbervera@yahoo.com>
2017-11-07 22:18:45 -0800

    MC Character Attacks and Fixed Tommy Workspace
    The animation for the MC attacking is in place, and I fixed the issue of
    Tommy having duplicate scripts, which caused issues on both of our
    scenes.

---
+++ Assets/Dzuy Workspace/Scripts/PlayerAttack.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+    private bool Attacking;
+
+    //attackTime is how long until attack is done
+    //set Attacking to false
+    //attackTimeCounter is the countdown
+    public float attackTime;
+
+    private float attackTimeCounter;
+    private GameObject player;
+    private Animator anim;
+    private Rigidbody2D playerRigid;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerRigid = player.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Comma) || Input.GetKeyDown(KeyCode.Period))
+        {
+            print("Here");
+            Vector2 faceDirection = player.GetComponent<PlayerMove>().GetLastMove();
+
+            //if player is just starting, set the attack direction to down (because the default sprite is looking down)
+            if (faceDirection == Vector2.zero)
+            {
+                faceDirection = new Vector2(0, -1);
+            }
+            attackTimeCounter = attackTime;
+            Attacking = true;
+
+            //don't want player to move while attacking
+            //so disable PlayerMove script while attacking
+            player.GetComponent<PlayerMove>().enabled = false;
+            playerRigid.velocity = Vector2.zero;
+
+            //animates movement based on direction facing
+            anim.SetBool("Attacking", true);
+            anim.SetFloat("Last Move X", faceDirection.x);
+            anim.SetFloat("Last Move Y", faceDirection.y);
+        }
+
+        //countdown to know when to end attack animation
+        if (attackTimeCounter > 0)
+        {
+            attackTimeCounter -= Time.deltaTime;
+        }
+
+        if (attackTimeCounter <= 0)
+        {
+            Attacking = false;
+            anim.SetBool("Attacking", false);
+            player.GetComponent<PlayerMove>().enabled = true;
+        }
+	}
+}
--- Assets/Dzuy Workspace/Scripts/PlayerMove.cs
+++ Assets/Dzuy Workspace/Scripts/PlayerMove.cs
@@ -10,22 +10,31 @@ public class PlayerMove : MonoBehaviour {
     private Animator anim;
     private bool playerMoving;
     private Vector2 lastMove;
-
+    private static bool playerExists;
 
 	void Start () {
         anim = GetComponent<Animator>();
         MCBody = GetComponent<Rigidbody2D>();
-        DontDestroyOnLoad(transform.gameObject);
+
+        if (!playerExists)
+        {
+            playerExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
         playerMoving = false;
-
+        
 		if (Input.GetAxisRaw("Horizontal") >= 0.5 || Input.GetAxisRaw("Horizontal") <= -0.5)
         {
-            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0.0f, 0.0f));
             MCBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, MCBody.velocity.y);
             playerMoving = true;
             lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
@@ -33,9 +42,7 @@ public class PlayerMove : MonoBehaviour {
 
         if (Input.GetAxisRaw("Vertical") >= 0.5 || Input.GetAxisRaw("Vertical") <= -0.5)
         {
-            //transform.Translate(new Vector3(0.0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0.0f));
             MCBody.velocity = new Vector2(MCBody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
-            //MCBody.velocity = new Vector2();
             playerMoving = true;
             lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
         }
@@ -58,6 +65,11 @@ public class PlayerMove : MonoBehaviour {
         anim.SetFloat("Move Y", Input.GetAxisRaw("Vertical"));
         anim.SetFloat("Last Move X", lastMove.x);
         anim.SetFloat("Last Move Y", lastMove.y);
+    }
 
+    //returns the player's last move (i.e. direction they are facing) for attacking script
+    public Vector2 GetLastMove()
+    {
+        return lastMove;
     }
 }
