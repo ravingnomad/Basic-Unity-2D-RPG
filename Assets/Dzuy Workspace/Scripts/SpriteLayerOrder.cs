63a05d455a38d47c35b9872c92b0f3e7a149ca98
unknown <joshbervera@yahoo.com>
2021-10-11 15:45:40 -0700

    Added SpriteLayerOrder Script
    SpriteLayerOrder changes a sprites layer order depending on their y-coordinate. This is so player and enemy sprites can go behind and in front of environment sprites.

---
+++ Assets/Dzuy Workspace/Scripts/SpriteLayerOrder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteLayerOrder : MonoBehaviour {
+
+	private SpriteRenderer spriteRender;
+	private Transform spriteTransform;
+
+	void Start () {
+		spriteTransform = GetComponent<Transform>();
+		spriteRender = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		spriteRender.sortingOrder = (int)(spriteTransform.position.y * -15f);
+	}
+}
